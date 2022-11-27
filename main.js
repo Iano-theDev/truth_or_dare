78a6997a4239dec631ae21a993e79790fbdaa106
Iano-theDev <sinceianmike@gmail.com>
2022-11-27 15:34:25 +0300

    Update status

--- main.js
+++ main.js
@@ -1,16 +1,21 @@
 const truthBtn = document.querySelector('#truthBtn');
 const dareBtn = document.querySelector('#dareBtn');
+const resetBtn = document.querySelector('#resetBtn');
 const respContainer = document.querySelector('#respContainer');
 const respContent = document.querySelector('#respContent');
 
 
 // document.getElementById('#respContainer').style.visibility = 'hidden';
 truthBtn.onclick = () => {
-    alert(`your truth challenge is on the way`);
-    respContent.textContent = `Most Embarasing moment!`;
+    //alert(`your truth challenge is on the way`);
+    respContent.textContent = `None rn, sigh!`;
   }
 
-  dareBtn.onclick = () => {
-    alert(`your dare is on the way`);
-    respContent.textContent = `You have to dance on the table!`;
+dareBtn.onclick = () => {
+    // alert(`your dare is on the way`);
+    respContent.textContent = `Hmmm, No dares for you rn!, Enjoy your peace!`;
   }
+
+resetBtn.onlick = () => {
+    window.location.reload();
+}
